1c281eb7cfd10447f6f3ad6a04d6d3bc66c5fadc
venkat11209 <venkateswarlu.p@fission.it>
2023-09-05 11:56:40 +0530

    mysql-server&mysql-client installation script file

---
+++ mysql-server.sh
@@ -0,0 +1,46 @@
+#############1.mysql-server&mysql-client installation:
+if [ -x "$(command -v mysql)" ]; then
+    version=$(mysql -V | awk '{print $3}' | awk -F',' '{print $1}')
+    if [ "$version" == "$version" ]; then
+        echo "MySQL $version is already installed."
+    else
+	  echo "display existed mysql-server version"
+      version1=$(mysql -V 2>&1 | awk '{print $5}')
+
+      # Extract the version number
+      version_number=$(echo "$version1" | awk -F. '{print $1}')
+
+      #Print the version information
+      echo "MySQL version: $version1"
+      echo "MySQL version number: $version_number"
+    fi
+else
+    echo "mysql is not installed. need to Installing mysql"
+    
+fi
+# Check if the MySQL root password is set
+if [ -x "$(command -v mysql_config_editor)" ]; then
+    
+    mysql_config_editor print --login-path=root > /dev/null 2>&1
+
+    if [ $? -eq 0 ]; then
+        echo "MySQL root password is already set."
+        echo "need to create db,db_user and db_password by manually"
+    fi
+else
+    echo "MySQL Server is not installed.install MySQL Server...."
+    # Install MySQL Server
+    sudo apt-get update
+    sudo apt-get install -y mysql-server
+    sudo apt-get install mysql-client -y
+
+    # Start and enable MySQL service
+    sudo systemctl start mysql
+    sudo systemctl enable mysql
+    echo "set root password when asking on command line"
+    read -sp "Enter new root password: " password
+    echo
+    mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${password}';"
+	mysql -uroot -p${password} -e "exit;"
+    echo "Root user does not exist.so Root user and password created successfully."
+ fi
