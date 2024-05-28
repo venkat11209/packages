d541398a2c9ca1088c146aeb39cc399a90dd970d
venkat11209 <venkateswarlu.p@fission.it>
2024-05-28 18:25:57 +0530

    script file for pmta-5.7

--- redis-server-uninstall.sh
+++
@@ -1,9 +0,0 @@
-#############3.redis uninstallation:
-if [ -x "$(command -v redis-cli)" ]; then
-    echo "Redis is already installed.uninstalling redis...."
-		sudo apt-get --purge remove redis-server -y
-        sudo rm -rf /etc/redis/dump.rdb
-        echo "redis is uninstalled successfully."
-else
-    echo "redis not found.need to install redis"
-fi 
